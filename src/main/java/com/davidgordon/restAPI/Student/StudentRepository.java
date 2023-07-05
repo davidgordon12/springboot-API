3605de6dd4a186fb950ce36a1c498bae41bcd3d3
davidgordon12 <davidgordon12@live.ca>
2023-07-05 19:49:06 -0400

    Add custom query

--- src/main/java/com/davidgordon/restAPI/Student/StudentRepository.java
+++ src/main/java/com/davidgordon/restAPI/Student/StudentRepository.java
@@ -1,10 +1,15 @@
 package com.davidgordon.restAPI.Student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository
         extends JpaRepository<Student, Integer> {
 
+    @Query("SELECT s FROM Student s WHERE s.lastName = ?1")
+    Optional<Student> findByLastName(String lastName);
 }
