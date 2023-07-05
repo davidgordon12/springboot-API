538585bf931e9e0f84cc1131fe3e8d82d39c2ce3
tehtko <moderndsnsam@gmail.com>
2023-07-05 14:26:38 -0400

    Inject studentRepository and move data access to seperate layer

--- src/main/java/com/davidgordon/restAPI/Student/StudentService.java
+++ src/main/java/com/davidgordon/restAPI/Student/StudentService.java
@@ -1,41 +1,21 @@
 package com.davidgordon.restAPI.Student;
 
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
-
-        students.add(new Student (
-                1,
-                "David",
-                "Gordon",
-                LocalDate.of(2002, 12, 13),
-                4.0f
-        ));
-
-        students.add(new Student (
-                2,
-                "Jane",
-                "Doe",
-                LocalDate.of(2001, 1, 19),
-                4.0f
-        ));
-
-        students.add(new Student (
-                3,
-                "Billy",
-                "Bob",
-                LocalDate.of(1999, 4, 27),
-                2.8f
-        ));
-
-        return students;
+        return studentRepository.findAll();
     }
 }
